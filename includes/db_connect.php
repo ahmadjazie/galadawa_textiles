24664fab85db5e1fedc1168eabcc36e0a71a7859
ahmadjazie <ahmadshuaibu065@gmail.com>
2026-05-26 01:01:22 +0100

    update online database connection

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,26 +1,16 @@
 <?php
 // DB_CONNECT.PHP
-// This file handles the connection to the MySQL database.
-// I include this file on every page that needs to talk to the database.
-
-// $servername = "localhost";
-// $username = "ameer";     // Default 
-// $password = "0556";         // Default 
-// $dbname = "galadawa_textile_db";
 
 $servername = "sql8.freesqldatabase.com";
 $username = "sql8828251";
 $password = "HSarGF9BxC";
 $dbname = "sql8828251";
 
-// I using MySQLi Object-Oriented style to create connection
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check if connection worked
+// Check connection
 if ($conn->connect_error) {
-    // If it failed, kill the page and show error
     die("Connection failed: " . $conn->connect_error);
 }
-
-// If the script continues past here, the database is connected!
 ?>
